be81c5d0a6a5ad0922318cdc4a685c3a66ed6bfa
Wojciech Oczkowski <wojciech.oczkowski@yougov.com>
2022-02-13 21:48:52 +0100

    Inject repository to controler classes

--- application/controllers.py
+++ application/controllers.py
@@ -1,7 +1,10 @@
 from dataclasses import dataclass
 
+from fastapi import Depends
+
 from application.domain import Game
 from application.domain import GameId
+from application.persistence import GameRepository
 
 
 @dataclass
@@ -10,20 +13,32 @@ class UpdateGameRequest:
 
 
 class AllGamesController:
+    def __init__(self, repository: GameRepository = Depends()) -> None:
+        self._repo = repository
+
     async def get_all(self) -> list[Game]:
         raise NotImplementedError
 
 
 class GetGameController:
+    def __init__(self, repository: GameRepository = Depends()) -> None:
+        self._repo = repository
+
     async def get(self, game_id: GameId) -> Game:
         raise NotImplementedError
 
 
 class DeleteGameController:
+    def __init__(self, repository: GameRepository = Depends()) -> None:
+        pass
+
     async def delete(self, game_id: GameId) -> None:
         raise NotImplementedError
 
 
 class UpdateGameController:
+    def __init__(self, repository: GameRepository = Depends()) -> None:
+        pass
+
     async def update(self, request: UpdateGameRequest) -> None:
         raise NotImplementedError
--- tests/test_controllers.py
+++ tests/test_controllers.py
@@ -1,39 +1,54 @@
 import pytest
+from mockito import mock
 
 from application.controllers import AllGamesController
 from application.controllers import DeleteGameController
 from application.controllers import GetGameController
 from application.controllers import UpdateGameController
 from application.domain import GameId
+from application.persistence import GameRepository
 from tests.factories import UpdateGameRequestFactory
 
 
+@pytest.fixture
+def repository() -> GameRepository:
+    return mock(GameRepository)
+
+
 @pytest.mark.asyncio
-async def test_all_games_controller_raises_on_get_all() -> None:
-    controller = AllGamesController()
+async def test_all_games_controller_raises_on_get_all(
+    repository: GameRepository,
+) -> None:
+    controller = AllGamesController(repository=repository)
     with pytest.raises(NotImplementedError):
         await controller.get_all()
 
 
 @pytest.mark.asyncio
-async def test_get_game_controller_raises_on_get() -> None:
-    controller = GetGameController()
+async def test_get_game_controller_raises_on_get(
+    repository: GameRepository,
+) -> None:
+    controller = GetGameController(repository=repository)
     game_id = GameId("foo")
     with pytest.raises(NotImplementedError):
         await controller.get(game_id)
 
 
 @pytest.mark.asyncio
-async def test_delete_game_controller_raises_on_delete() -> None:
-    controller = DeleteGameController()
+async def test_delete_game_controller_raises_on_delete(
+    repository: GameRepository,
+) -> None:
+    controller = DeleteGameController(repository=repository)
     game_id = GameId("foo")
     with pytest.raises(NotImplementedError):
         await controller.delete(game_id)
 
 
 @pytest.mark.asyncio
-async def test_update_game_controller_raises_on_update() -> None:
-    controller = UpdateGameController()
+async def test_update_game_controller_raises_on_update(
+    repository: GameRepository,
+) -> None:
+    controller = UpdateGameController(repository=repository)
     request = UpdateGameRequestFactory()
     with pytest.raises(NotImplementedError):
         await controller.update(request=request)
