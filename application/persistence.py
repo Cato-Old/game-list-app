056553a98a2a14f41622525ea0fa1221e554a6c1
Wojciech Oczkowski <wojciech.oczkowski@yougov.com>
2022-02-13 20:26:24 +0100

    Add stub for get_one repository method

--- application/persistence.py
+++ application/persistence.py
@@ -1,6 +1,10 @@
 from application.domain import Game
+from application.domain import GameId
 
 
 class GameRepository:
     async def get_all(self) -> list[Game]:
+        raise NotImplementedError
+
+    async def get_one(self, game_id: GameId) -> Game:
         raise NotImplementedError
--- tests/test_persistence.py
+++ tests/test_persistence.py
@@ -1,5 +1,6 @@
 import pytest
 
+from application.domain import GameId
 from application.persistence import GameRepository
 
 
@@ -12,3 +13,10 @@ def repository() -> GameRepository:
 async def test_raises_on_get_all(repository: GameRepository) -> None:
     with pytest.raises(NotImplementedError):
         await repository.get_all()
+
+
+@pytest.mark.asyncio
+async def test_raises_on_get_one(repository: GameRepository) -> None:
+    game_id = GameId("foo")
+    with pytest.raises(NotImplementedError):
+        await repository.get_one(game_id)
