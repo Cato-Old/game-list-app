403eae86bc687b30bd691945979ba5915028f93f
Wojciech Oczkowski <wojciech.oczkowski@yougov.com>
2022-02-13 17:02:43 +0100

    Add stub for GetGameController

--- application/controllers.py
+++ application/controllers.py
@@ -1,6 +1,12 @@
 from application.domain import Game
+from application.domain import GameId
 
 
 class AllGamesController:
     async def get_all(self) -> list[Game]:
+        raise NotImplementedError
+
+
+class GetGameController:
+    async def get(self, game_id: GameId) -> Game:
         raise NotImplementedError
--- tests/test_controllers.py
+++ tests/test_controllers.py
@@ -1,6 +1,8 @@
 import pytest
 
 from application.controllers import AllGamesController
+from application.controllers import GetGameController
+from application.domain import GameId
 
 
 @pytest.mark.asyncio
@@ -8,3 +10,11 @@ async def test_all_games_controller_raises_on_get_all() -> None:
     controller = AllGamesController()
     with pytest.raises(NotImplementedError):
         await controller.get_all()
+
+
+@pytest.mark.asyncio
+async def test_get_game_controller_raises_on_get() -> None:
+    controller = GetGameController()
+    game_id = GameId("foo")
+    with pytest.raises(NotImplementedError):
+        await controller.get(game_id)
