b32c6587235d23c3233b2920fece42d53aac9d96
Wojciech Oczkowski <wojciech.oczkowski@yougov.com>
2022-02-13 16:55:41 +0100

    Integrate all_games controller with endpoint

--- application/endpoints.py
+++ application/endpoints.py
@@ -1,14 +1,20 @@
 from http import HTTPStatus
 
 from fastapi import APIRouter
+from fastapi import Depends
 from fastapi import HTTPException
+
+from application.controllers import AllGamesController
 
 router = APIRouter()
 
 
 @router.get("/games/")
-async def get_all_games() -> None:
-    raise HTTPException(status_code=HTTPStatus.NOT_IMPLEMENTED)
+async def get_all_games(controller: AllGamesController = Depends()) -> None:
+    try:
+        await controller.get_all()
+    except NotImplementedError:
+        raise HTTPException(status_code=HTTPStatus.NOT_IMPLEMENTED)
 
 
 @router.post("/game/{game_id}/")
