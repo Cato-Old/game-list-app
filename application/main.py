6a48359986b79efa50c947bced9563839ca35bcf
Wojciech Oczkowski <wojciech.oczkowski@yougov.com>
2022-02-14 15:54:37 +0100

    Startup hook for connection to MongoDB

--- application/main.py
+++ application/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
 from application import endpoints
+from application.dependencies import setup_mongo_client
 
 app = FastAPI()
 
 app.include_router(endpoints.router)
+app.add_event_handler("startup", setup_mongo_client)
--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,44 +1,42 @@
 from http import HTTPStatus
 
 import pytest
+from asgi_lifespan import LifespanManager
 from httpx import AsyncClient
-from starlette.testclient import TestClient
 
 from application.main import app
 from tests.factories import GameFactory
 
 
 @pytest.fixture
-def client() -> TestClient:
-    return TestClient(app)
-
-
-@pytest.mark.asyncio
-async def test_returns_501_on_get_all_games(client: TestClient) -> None:
+async def client() -> AsyncClient:
     async with AsyncClient(app=app, base_url="http://test") as client:
-        result = await client.get("/games/")
+        async with LifespanManager(app):
+            yield client
+
+
+@pytest.mark.anyio
+async def test_returns_501_on_get_all_games(client: AsyncClient) -> None:
+    result = await client.get("/games/")
     assert HTTPStatus.NOT_IMPLEMENTED == result.status_code
 
 
-@pytest.mark.asyncio
-async def test_returns_501_on_get_game(client: TestClient) -> None:
+@pytest.mark.anyio
+async def test_returns_501_on_get_game(client: AsyncClient) -> None:
     game = GameFactory()
-    async with AsyncClient(app=app, base_url="http://test") as client:
-        result = await client.post(f"/game/{game.id}/")
+    result = await client.post(f"/game/{game.id}/")
     assert HTTPStatus.NOT_IMPLEMENTED == result.status_code
 
 
-@pytest.mark.asyncio
-async def test_returns_501_on_delete_game(client: TestClient) -> None:
+@pytest.mark.anyio
+async def test_returns_501_on_delete_game(client: AsyncClient) -> None:
     game = GameFactory()
-    async with AsyncClient(app=app, base_url="http://test") as client:
-        result = await client.delete(f"/game/{game.id}/")
+    result = await client.delete(f"/game/{game.id}/")
     assert HTTPStatus.NOT_IMPLEMENTED == result.status_code
 
 
-@pytest.mark.asyncio
-async def test_returns_501_on_update_game(client: TestClient) -> None:
+@pytest.mark.anyio
+async def test_returns_501_on_update_game(client: AsyncClient) -> None:
     game = GameFactory()
-    async with AsyncClient(app=app, base_url="http://test") as client:
-        result = await client.put(f"/game/{game.id}/")
+    result = await client.put(f"/game/{game.id}/")
     assert HTTPStatus.NOT_IMPLEMENTED == result.status_code
