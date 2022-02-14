5e61805098e290f07587b7979fbf3137305e4e74
Wojciech Oczkowski <wojciech.oczkowski@yougov.com>
2022-02-14 12:06:11 +0100

    Use httpx as client for endpoint texts

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,35 +1,44 @@
 from http import HTTPStatus
 
-from pytest import fixture
+import pytest
+from httpx import AsyncClient
 from starlette.testclient import TestClient
 
 from application.main import app
 from tests.factories import GameFactory
 
 
-@fixture
+@pytest.fixture
 def client() -> TestClient:
     return TestClient(app)
 
 
-def test_returns_501_on_get_all_games(client: TestClient) -> None:
-    result = client.get("/games/")
+@pytest.mark.asyncio
+async def test_returns_501_on_get_all_games(client: TestClient) -> None:
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        result = await client.get("/games/")
     assert HTTPStatus.NOT_IMPLEMENTED == result.status_code
 
 
-def test_returns_501_on_get_game(client: TestClient) -> None:
+@pytest.mark.asyncio
+async def test_returns_501_on_get_game(client: TestClient) -> None:
     game = GameFactory()
-    result = client.post(f"/game/{game.id}/")
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        result = await client.post(f"/game/{game.id}/")
     assert HTTPStatus.NOT_IMPLEMENTED == result.status_code
 
 
-def test_returns_501_on_delete_game(client: TestClient) -> None:
+@pytest.mark.asyncio
+async def test_returns_501_on_delete_game(client: TestClient) -> None:
     game = GameFactory()
-    result = client.delete(f"/game/{game.id}/")
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        result = await client.delete(f"/game/{game.id}/")
     assert HTTPStatus.NOT_IMPLEMENTED == result.status_code
 
 
-def test_returns_501_on_update_game(client: TestClient) -> None:
+@pytest.mark.asyncio
+async def test_returns_501_on_update_game(client: TestClient) -> None:
     game = GameFactory()
-    result = client.put(f"/game/{game.id}/")
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        result = await client.put(f"/game/{game.id}/")
     assert HTTPStatus.NOT_IMPLEMENTED == result.status_code
